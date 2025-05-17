d14f06ed45eaf12a57c62a1a75c8d3d832fa26fc
xiao <scarletsheep99@gmail.com>
2025-05-17 16:07:58 +0800

    modify

--- lesson6/lesson6_3.py
+++ lesson6/lesson6_3.py
@@ -1,24 +1,34 @@
 import random
-min = 1
-max = 99
-random_number = random.randint(min,max)
-count = 0
-print(random_number)
-print("=======Guess Game Start=======\n\n")
-while True:
-  input_number = int(input(f"Input number({min})~({max}):"))
-  count += 1
-  if (input_number == random_number):
-    print(f"BINGO!,The answer is {input_number}")
-    print(f"u guess {count} times")
-    break
-  elif (input_number > random_number):
-    print(f"Too Big !")
-    max = input_number - 1
-  elif (input_number < random_number):
-    print(f"Too Small !")
-    min = input_number + 1
-  print(f"u guessed {count} times")
 
-print("Congratulations!")
-print("^___^")
+def guess_number():
+  min = 1
+  max = 99
+  random_number = random.randint(min,max)
+  count = 0
+  print(random_number)
+  print("=======Guess Game Start=======\n\n")
+  while True:
+    input_number = int(input(f"Input number({min})~({max}):"))
+    count += 1
+    if (input_number == random_number):
+      print(f"BINGO!,The answer is {input_number}")
+      print(f"u guess {count} times")
+      break
+    elif (input_number > random_number):
+      print(f"Too Big !")
+      max = input_number - 1
+    elif (input_number < random_number):
+      print(f"Too Small !")
+      min = input_number + 1
+    print(f"u guessed {count} times")
+
+  print("Congratulations!")
+  print("^___^")
+
+while(True):
+  guess_number()
+  guess_again = input("Again?(y,n):")
+  if(guess_again == "n"):
+    break
+  
+print("ByeBye")
