26624c412ed74a5716df129e34f1aec2eae853c8
xiao <scarletsheep99@gmail.com>
2025-05-24 14:47:30 +0800

    modify

--- lesson8/lesson8_1.py
+++ lesson8/lesson8_1.py
@@ -1,15 +1,19 @@
+#import edu
+#from edu.tools import caculate_bmi, classify_bmi
+from edu.tools import caculate_bmi as a1
+from edu.tools import classify_bmi as a2
 import tools
 
 def main():
-    #height = 170
-    height:int = int(input("請輸入身高(cm):"))
-    #weight = 62
-    weight:int = int(input("請輸入體重(kg):"))
+    height = 170
+    #height:int = int(input("請輸入身高(cm):"))
+    weight = 63
+    #weight:int = int(input("請輸入體重(kg):"))
 
-    bmi = tools.caculate_bmi(height , weight)
+    bmi = a1(height , weight)
 
     print(f"BMI:{bmi:.2f}")
-    print(tools.classify_bmi(bmi))
+    print(a2(bmi))
 
 
 if __name__ == '__main__':
