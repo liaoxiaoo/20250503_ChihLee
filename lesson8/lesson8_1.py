ca8f171a359a4c64c3bb13859c1f3b220a06a880
xiao <scarletsheep99@gmail.com>
2025-05-24 14:15:38 +0800

    modify

---
+++ lesson8/lesson8_1.py
@@ -0,0 +1,16 @@
+import tools
+
+def main():
+    #height = 170
+    height:int = int(input("請輸入身高(cm):"))
+    #weight = 62
+    weight:int = int(input("請輸入體重(kg):"))
+
+    bmi = tools.caculate_bmi(height , weight)
+
+    print(f"BMI:{bmi:.2f}")
+    print(tools.classify_bmi(bmi))
+
+
+if __name__ == '__main__':
+    main()
